d7bd3ec09902520e3b33bb116efa74760ef58b2b
drishu <aszilagyi@live.com>
2020-04-16 10:18:16 +0000

    OPENEUROPA-3044: Change controller and set corporate_default.

---
+++ src/Routing/DecoratingRouteSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\oe_contact_forms\Routing;
+
+use Symfony\Component\Routing\RouteCollection;
+use \Drupal\contact_storage\Controller\ContactStorageController;
+use Drupal\contact_storage\Routing\RouteSubscriber;
+use Drupal\oe_contact_forms\Controller\OeContactFormsController;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class DecoratingRouteSubscriber extends RouteSubscriber {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    parent::alterRoutes($collection);
+    
+    // Change the contact_form controller.
+    if ($route = $collection->get('entity.contact_form.canonical')) {
+      $route->setDefault('_controller', OeContactFormsController::class . '::contactSitePage');
+    }
+  }
+
+}

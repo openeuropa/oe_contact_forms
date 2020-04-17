61d37c435fb917589046d1912d7431746dc2631b
drishu <aszilagyi@live.com>
2020-04-17 12:14:29 +0000

    OPENEUROPA-3042: Kernel test fixes, add sparcl to phpunit.

--- tests/Kernel/BaseFieldTest.php
+++ tests/Kernel/BaseFieldTest.php
@@ -2,30 +2,31 @@
 
 declare(strict_types = 1);
 
-use Drupal\Tests\BrowserTestBase;
+namespace Drupal\Tests\oe_contact_forms\Kernel;
+
+use Drupal\Tests\rdf_entity\Kernel\RdfKernelTestBase;
 use Drupal\contact\Entity\ContactForm;
 use Drupal\contact\Entity\Message;
 
 /**
  * Tests contact form with new fields.
  */
-class BaseFieldTest extends BrowserTestBase {
-
-  /**
-   * An test user with permission to submit contact forms.
-   *
-   * @var \Drupal\user\UserInterface
-   */
-  protected $testuser;
+class BaseFieldTest extends RdfKernelTestBase {
 
   /**
    * {@inheritdoc}
    */
-  protected static $modules = [
+  public static $modules = [
     'user',
     'system',
+    'field',
+    'options',
+    'views',
+    'telephone',
     'contact',
     'contact_storage',
+    'rdf_entity',
+    'rdf_skos',
     'oe_contact_forms',
   ];
 
@@ -35,32 +36,8 @@ class BaseFieldTest extends BrowserTestBase {
   protected function setUp() {
     parent::setUp();
 
-    // Create and login test user.
-    $this->testuser = $this->drupalCreateUser([
-      'access site-wide contact form',
-    ]);
-    $this->drupalLogin($this->testuser);
-  }
-
-  /**
-   * Tests the field validation for corporate forms.
-   */
-  public function testCorporateFormFieldValidation(): void {
-    $contact_form_id = 'oe_contact_form';
-    $contact_form = ContactForm::create(['id' => $contact_form_id]);
-    $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', TRUE);
-    $contact_form->save();
-
-    $this->drupalGet('contact/' . $contact_form_id);
-    $this->assertSession()->statusCodeEquals(200);
-
-    $edit = [
-      'subject[0][value]' => 'Test subject',
-      'message[0][value]' => 'Test message',
-    ];
-
-    $this->drupalPostForm('contact/' . $contact_form_id, $edit, t('Send message'));
-    $this->assertText(t('@name field is required.', ['@name' => 'Topic']));
+    $this->installEntitySchema('contact_message');
+    $this->installConfig(['field', 'system']);
   }
 
   /**
@@ -89,31 +66,15 @@ class BaseFieldTest extends BrowserTestBase {
 
     $message = Message::create($data);
     $message->save();
-    $message = Message::load(1);
+
+    $entity_type_manager = \Drupal::entityTypeManager()->getStorage('contact_message');
+    $entity_type_manager->resetCache();
+    /** @var \Drupal\contact\Entity\Message $message */
+    $message = $entity_type_manager->load($message->id());
 
     $this->assertEquals($data['oe_country_residence'], $message->get('oe_country_residence')->getValue()['0']['target_id']);
     $this->assertEquals($data['oe_telephone'], $message->get('oe_telephone')->getValue()['0']['value']);
     $this->assertEquals($data['oe_topic'], $message->get('oe_topic')->getValue()['0']['value']);
-  }
-
-  /**
-   * Tests the field absence for default forms.
-   */
-  public function testDefaultFormFieldAbsence(): void {
-    $contact_form_id = 'default_form';
-    $contact_form = ContactForm::create(['id' => $contact_form_id]);
-    $contact_form->save();
-
-    $this->drupalGet('contact/' . $contact_form_id);
-    $this->assertSession()->statusCodeEquals(200);
-
-    $edit = [
-      'subject[0][value]' => 'Test subject',
-      'message[0][value]' => 'Test message',
-    ];
-
-    $this->drupalPostForm('contact/' . $contact_form_id, $edit, t('Send message'));
-    $this->assertNoText(t('@name field is required.', ['@name' => 'Topic']));
   }
 
 }

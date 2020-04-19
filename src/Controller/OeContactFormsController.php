5e7d2523e9473bcb5e9a75b8e137e66b121c5308
drishu <aszilagyi@live.com>
2020-04-19 10:09:41 +0000

    OPENEUROPA-3044: Standards.

--- src/Controller/OeContactFormsController.php
+++ src/Controller/OeContactFormsController.php
@@ -1,11 +1,10 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\oe_contact_forms\Controller;
 
 use Drupal\contact\ContactFormInterface;
-use Drupal\contact\Controller\ContactController;
-use Drupal\Core\Url;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Drupal\contact_storage\Controller\ContactStorageController;
 
 /**
@@ -17,15 +16,21 @@ class OeContactFormsController extends ContactStorageController {
    * {@inheritdoc}
    */
   public function contactSitePage(ContactFormInterface $contact_form = NULL) {
+    $config = $this->config('contact.settings');
     // If the requested contact form is corporate,
     // render it with the corporate form handler ("corporate_default"),
-    // instead of the default one. 
+    // instead of the default one.
     if (!empty($contact_form) && $contact_form->getThirdPartySetting('oe_contact_forms', 'is_corporate_form', FALSE)) {
-      $contact_form->setHandlerClass('corporate_default', '\Drupal\oe_contact_forms\...');
-      $manager = $this->entityTypeManager();
-      $view_builder = $manager->getViewBuilder('contact_form');
+      $message = $this->entityTypeManager()->getStorage('contact_message')->create([
+        'contact_form' => $contact_form->id(),
+      ]);
 
-      return $view_builder->view($contact_form, 'corporate_default', $contact_form->language());
+      $form = $this->entityFormBuilder()->getForm($message, 'corporate_default');
+      $form['#title'] = $contact_form->label();
+      $form['#cache']['contexts'][] = 'user.permissions';
+      $this->renderer->addCacheableDependency($form, $config);
+
+      return $form;
     }
 
     return parent::contactSitePage($contact_form);

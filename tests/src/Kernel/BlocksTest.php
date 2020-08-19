ce483d7b93788863c4d1aae187258b2b71fcb908
upchuk <danny@webomelette.com>
2020-08-19 11:46:45 +0200

    EWPP-177: Improving the block test inline comments.

--- tests/src/Kernel/BlocksTest.php
+++ tests/src/Kernel/BlocksTest.php
@@ -18,7 +18,8 @@ class BlocksTest extends ContactFormTestBase {
     /** @var \Drupal\Core\Block\BlockManagerInterface $block_manager */
     $block_manager = $this->container->get('plugin.manager.block');
 
-    $contact_form = ContactForm::create(['id' => 'oe_contact_form_compare']);
+    // Create an initial contact form with one topic.
+    $contact_form = ContactForm::create(['id' => 'oe_contact_form_one']);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', TRUE);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'expose_as_block', TRUE);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'privacy_policy', 'http://example.net');
@@ -37,7 +38,8 @@ class BlocksTest extends ContactFormTestBase {
     // Assert we have correct topic option.
     $this->assertContains('Topic to compare', $build['oe_topic']['widget']['#options']);
 
-    $contact_form = ContactForm::create(['id' => 'oe_contact_form']);
+    // Create a second contact form, this time with another topic.
+    $contact_form = ContactForm::create(['id' => 'oe_contact_form_two']);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', TRUE);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'expose_as_block', TRUE);
     $topic_label = 'Topic label';
@@ -59,48 +61,53 @@ class BlocksTest extends ContactFormTestBase {
     $plugin = $block_manager->createInstance($plugin_id);
     $build = $plugin->build();
 
-    // Assert we have correct build.
+    // Assert we have the correct build.
     $this->assertEqual($build['header']['#markup'], $header);
     $this->assertEqual($build['privacy_policy']['#title'], $privacy_text);
     $this->assertEqual($build['oe_topic']['widget']['#title'], $topic_label);
     $this->assertContains('Topic name', $build['oe_topic']['widget']['#options']);
+
+    // Assert the dynamically generated topics did not get cached and we only
+    // have the topic configured on this contact form.
     $this->assertNotContains('Topic to compare', $build['oe_topic']['widget']['#options']);
 
-    // Remove from derivatives.
+    // Configure the form not to be exposed as a block anymore.
     $contact_form->setThirdPartySetting('oe_contact_forms', 'expose_as_block', FALSE);
     $contact_form->save();
 
-    // Assert the block was removed.
+    // Assert the contact form is no longer derived into a block.
     $this->assertFalse($block_manager->hasDefinition($plugin_id));
 
-    $contact_form = ContactForm::create(['id' => 'oe_contact_form_no_block']);
+    // Create a third contact form and don't expose it as a block.
+    $contact_form = ContactForm::create(['id' => 'oe_contact_form_three']);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', TRUE);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'expose_as_block', FALSE);
     $contact_form->save();
     $plugin_id = 'oe_contact_forms_corporate_block:' . $contact_form->uuid();
 
-    // Assert the block was not created.
+    // Assert the block derivative was not created.
     $this->assertFalse($block_manager->hasDefinition($plugin_id));
 
     // Now expose the block to pick up the change.
     $contact_form->setThirdPartySetting('oe_contact_forms', 'expose_as_block', TRUE);
     $contact_form->save();
 
-    // Assert the block was created.
+    // Assert the block derivative was created.
     $this->assertTrue($block_manager->hasDefinition($plugin_id));
 
     // Now delete the block content entity.
     $contact_form->delete();
 
-    // Assert the block was removed.
+    // Assert the block derivative was removed.
     $this->assertFalse($block_manager->hasDefinition($plugin_id));
 
-    $contact_form = ContactForm::create(['id' => 'default_contact_form']);
+    // Create a fourth contact form which is not corporate.
+    $contact_form = ContactForm::create(['id' => 'oe_contact_form_four']);
     $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', FALSE);
     $contact_form->save();
     $plugin_id = 'oe_contact_forms_corporate_block:' . $contact_form->uuid();
 
-    // Assert the block was not created.
+    // Assert the block derivative was not created.
     $this->assertFalse($block_manager->hasDefinition($plugin_id));
   }
 

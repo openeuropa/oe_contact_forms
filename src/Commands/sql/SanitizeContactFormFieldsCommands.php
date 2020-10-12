ca388d70f6f11fe6fdcf86625c8d375d4656ce4c
George Baev <geobaev@gmail.com>
2020-10-12 15:52:06 +0300

    D8TEWPPE-107: Add Contact Messages sanitization.

---
+++ src/Commands/sql/SanitizeContactFormFieldsCommands.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_contact_forms\Commands\sql;
+
+use Drush\Commands\DrushCommands;
+use Symfony\Component\Console\Input\InputInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Extension\ModuleHandler;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+/**
+ * Sanitizes the contact forms related data.
+ */
+class SanitizeContactFormFieldsCommands extends DrushCommands {
+
+  /**
+   * The database service.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandler
+   */
+  protected $moduleHandler;
+
+  /**
+   * The logger service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * SanitizeContactFormFieldsCommands constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database service.
+   * @param \Drupal\Core\Extension\ModuleHandler $moduleHandler
+   *   The module handler service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
+   *   The logger service.
+   */
+  public function __construct(
+    Connection $database,
+    ModuleHandler $moduleHandler,
+    LoggerChannelFactoryInterface $loggerFactory) {
+    $this->database = $database;
+    $this->moduleHandler = $moduleHandler;
+    $this->logger = $loggerFactory->get('oe_contact_forms');
+  }
+
+  /**
+   * Sanitize the contact data from the DB.
+   *
+   * @hook post-command sql-sanitize
+   *
+   * @inheritdoc
+   */
+  public function sanitize() {
+    $this->database->update('contact_message')
+      ->expression('name', "CONCAT('User', id)")
+      ->expression('mail', "CONCAT('user+', id, '@example.com')")
+      ->expression('subject', "CONCAT('subject-by-', id)")
+      ->expression('message', "CONCAT('message-by-', id)")
+      ->expression('oe_country_residence', "CONCAT('residence-in-', id)")
+      ->expression('oe_telephone', "CONCAT('+000-', id)")
+      ->expression('oe_topic', "CONCAT('topic-', id)")
+      ->fields([
+        'ip_address' => '127.0.0.1',
+      ])
+      ->execute();
+
+    $this->logger->notice('Contact messages data sanitized');
+  }
+
+  /**
+   * Sets the output message.
+   *
+   * @hook on-event sql-sanitize-confirms
+   *
+   * @inheritdoc
+   */
+  public function messages(&$messages, InputInterface $input) {
+    $messages[] = dt('Sanitize contact form data.');
+  }
+
+}
---
+++ tests/src/Kernel/SanitizeContactFormFieldsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_contact_forms\Kernel;
+
+use Drupal\contact\Entity\ContactForm;
+use Drupal\contact\Entity\Message;
+
+/**
+ * Tests the message fields drush sanitization.
+ */
+class SanitizeContactFormFieldsTest extends ContactFormTestBase {
+
+  /**
+   * Tests the drush sanitization.
+   */
+  public function testContactFormMessageDataSanitization() {
+    $contact_form_id = 'oe_contact_form';
+    $contact_form = ContactForm::create(['id' => $contact_form_id]);
+    $contact_form->setThirdPartySetting('oe_contact_forms', 'is_corporate_form', TRUE);
+    $contact_form->setThirdPartySetting('oe_contact_forms', 'topics', [
+      [
+        'topic_name' => 'Topic name',
+        'topic_email_address' => 'topic@emailaddress.com',
+      ],
+    ]);
+    $contact_form->save();
+
+    $data = [
+      'contact_form' => $contact_form->id(),
+      'name' => 'example',
+      'mail' => 'admin@example.com',
+      'created' => time(),
+      'ip_address' => '8.8.8.8',
+      'subject' => 'Test subject',
+      'message' => 'Test message',
+      'oe_country_residence' => 'http://publications.europa.eu/resource/authority/country/BEL',
+      'oe_telephone' => '0123456',
+      'oe_topic' => 'Topic name',
+    ];
+
+    $message = Message::create($data);
+
+    $message->save();
+
+    $message_id = $message->id();
+
+    $this->assertEqual(1, $message_id);
+
+    /** @var \Drupal\oe_contact_forms\Commands\sql\SanitizeContactFormFieldsCommands $command */
+    $command = \Drupal::service('oe_contact_forms.contact.commands');
+    $command->sanitize();
+
+    $sanitized_message = Message::load($message_id);
+
+    $residence = $sanitized_message->get('oe_country_residence')->getValue();
+
+    $this->assertEqual('User' . $message_id, $sanitized_message->name->value);
+    $this->assertEqual('user+' . $message_id . '@example.com', $sanitized_message->mail->value);
+    $this->assertEqual('subject-by-' . $message_id, $sanitized_message->subject->value);
+    $this->assertEqual('message-by-' . $message_id, $sanitized_message->message->value);
+    $this->assertEqual('127.0.0.1', $sanitized_message->ip_address->value);
+    $this->assertEqual('residence-in-' . $message_id, $sanitized_message->oe_country_residence->target_id);
+    $this->assertEqual('+000-' . $message_id, $sanitized_message->oe_telephone->value);
+    $this->assertEqual('topic-' . $message_id, $sanitized_message->oe_topic->value);
+  }
+
+}

d5462593069ae9169f53dd5f132892df2179292e
drishu <aszilagyi@live.com>
2020-10-21 07:52:04 +0000

    EWPP-376: Dont sanitize plain contact forms.

--- src/Commands/sql/SanitizeContactFormFieldsCommands.php
+++ src/Commands/sql/SanitizeContactFormFieldsCommands.php
@@ -9,8 +9,9 @@ use Drush\Drupal\Commands\sql\SanitizePluginInterface;
 use Consolidation\AnnotatedCommand\CommandData;
 use Symfony\Component\Console\Input\InputInterface;
 use Drupal\Core\Database\Connection;
-use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
 
 /**
  * Sanitizes the contact forms related data.
@@ -25,11 +26,11 @@ class SanitizeContactFormFieldsCommands extends DrushCommands implements Sanitiz
   protected $database;
 
   /**
-   * The module handler service.
+   * The entity type manager.
    *
-   * @var \Drupal\Core\Extension\ModuleHandler
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $moduleHandler;
+  protected $entityTypeManager;
 
   /**
    * The logger service.
@@ -39,19 +40,29 @@ class SanitizeContactFormFieldsCommands extends DrushCommands implements Sanitiz
   protected $logger;
 
   /**
+   * The entity cache service.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $entityCache;
+
+  /**
    * SanitizeContactFormFieldsCommands constructor.
    *
    * @param \Drupal\Core\Database\Connection $database
    *   The database service.
-   * @param \Drupal\Core\Extension\ModuleHandler $moduleHandler
-   *   The module handler service.
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
    *   The logger service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $entityCache
+   *   The entity cache service.
    */
-  public function __construct(Connection $database, ModuleHandler $moduleHandler, LoggerChannelFactoryInterface $loggerFactory) {
+  public function __construct(Connection $database, LoggerChannelFactoryInterface $loggerFactory, EntityTypeManagerInterface $entityTypeManager, CacheBackendInterface $entityCache) {
     $this->database = $database;
-    $this->moduleHandler = $moduleHandler;
+    $this->entityTypeManager = $entityTypeManager;
     $this->logger = $loggerFactory->get('oe_contact_forms');
+    $this->entityCache = $entityCache;
   }
 
   /**
@@ -62,18 +73,41 @@ class SanitizeContactFormFieldsCommands extends DrushCommands implements Sanitiz
    * @inheritdoc
    */
   public function sanitize($result, CommandData $commandData) {
-    $this->database->update('contact_message')
-      ->expression('name', "CONCAT('User', id)")
-      ->expression('mail', "CONCAT('user+', id, '@example.com')")
-      ->expression('subject', "CONCAT('subject-by-', id)")
-      ->expression('message', "CONCAT('message-by-', id)")
-      ->expression('oe_country_residence', "CONCAT('residence-in-', id)")
-      ->expression('oe_telephone', "CONCAT('+000-', id)")
-      ->expression('oe_topic', "CONCAT('topic-', id)")
-      ->fields([
-        'ip_address' => '127.0.0.1',
-      ])
-      ->execute();
+    /** @var \Drupal\contact\ContactFormInterface[] $contact_forms */
+    $contact_forms = $this->entityTypeManager->getStorage('contact_form')->loadMultiple();
+
+    foreach ($contact_forms as $key => $contact_form) {
+      $is_corporate_form = $contact_form->getThirdPartySetting('oe_contact_forms', 'is_corporate_form', FALSE);
+
+      if ($is_corporate_form) {
+        $this->database->update('contact_message')
+          ->expression('name', "CONCAT('User', id)")
+          ->expression('mail', "CONCAT('user+', id, '@example.com')")
+          ->expression('subject', "CONCAT('subject-by-', id)")
+          ->expression('message', "CONCAT('message-by-', id)")
+          ->expression('oe_country_residence', "CONCAT('residence-in-', id)")
+          ->expression('oe_telephone', "CONCAT('+000-', id)")
+          ->expression('oe_topic', "CONCAT('topic-', id)")
+          ->fields([
+            'ip_address' => '127.0.0.1',
+          ])
+          ->condition('contact_form', $contact_form->id(), '=')
+          ->execute();
+
+        $select = $this->database->select('contact_message', 'cm');
+        $select->fields('cm', ['id']);
+        $select->condition('contact_form', $contact_form->id(), '=');
+        $results = $select->execute()->fetchAll();
+
+        $cids = [];
+
+        foreach ($results as $result) {
+          $cids[] = 'values:contact_message:' . $result->id;
+        }
+
+        $this->entityCache->invalidateMultiple($cids);
+      }
+    }
 
     $this->logger->notice('Contact messages data sanitized.');
   }
--- tests/src/Kernel/SanitizeContactFormFieldsTest.php
+++ tests/src/Kernel/SanitizeContactFormFieldsTest.php
@@ -29,7 +29,7 @@ class SanitizeContactFormFieldsTest extends ContactFormTestBase {
     $contact_form->save();
 
     $data = [
-      'contact_form' => $contact_form->id(),
+      'contact_form' => $contact_form_id,
       'name' => 'example',
       'mail' => 'admin@example.com',
       'created' => time(),
@@ -43,9 +43,25 @@ class SanitizeContactFormFieldsTest extends ContactFormTestBase {
 
     $message = Message::create($data);
     $message->save();
-
     $message_id = $message->id();
-    $this->assertEqual(1, $message_id);
+
+    $plain_contact_form_id = 'plain_contact_form';
+    $plain_contact_form = ContactForm::create(['id' => $plain_contact_form_id]);
+    $plain_contact_form->save();
+
+    $data = [
+      'contact_form' => $plain_contact_form_id,
+      'name' => 'example',
+      'mail' => 'admin@example.com',
+      'created' => time(),
+      'ip_address' => '8.8.8.9',
+      'subject' => 'Test subject plain',
+      'message' => 'Test message plain',
+    ];
+
+    $plain_message = Message::create($data);
+    $plain_message->save();
+    $plain_message_id = $plain_message->id();
 
     /** @var \Drupal\oe_contact_forms\Commands\sql\SanitizeContactFormFieldsCommands $command */
     $command = \Drupal::service('oe_contact_forms.contact.sanitize_commands');
@@ -53,16 +69,22 @@ class SanitizeContactFormFieldsTest extends ContactFormTestBase {
 
     $sanitized_message = Message::load($message_id);
 
-    $residence = $sanitized_message->get('oe_country_residence')->getValue();
+    $this->assertEqual($sanitized_message->get('name')->value, 'User' . $message_id);
+    $this->assertEqual($sanitized_message->get('mail')->value, 'user+' . $message_id . '@example.com');
+    $this->assertEqual($sanitized_message->get('subject')->value, 'subject-by-' . $message_id);
+    $this->assertEqual($sanitized_message->get('message')->value, 'message-by-' . $message_id);
+    $this->assertEqual($sanitized_message->get('ip_address')->value, '127.0.0.1');
+    $this->assertEqual($sanitized_message->get('oe_country_residence')->target_id, 'residence-in-' . $message_id);
+    $this->assertEqual($sanitized_message->get('oe_telephone')->value, '+000-' . $message_id);
+    $this->assertEqual($sanitized_message->get('oe_topic')->value, 'topic-' . $message_id);
 
-    $this->assertEqual('User' . $message_id, $sanitized_message->name->value);
-    $this->assertEqual('user+' . $message_id . '@example.com', $sanitized_message->mail->value);
-    $this->assertEqual('subject-by-' . $message_id, $sanitized_message->subject->value);
-    $this->assertEqual('message-by-' . $message_id, $sanitized_message->message->value);
-    $this->assertEqual('127.0.0.1', $sanitized_message->ip_address->value);
-    $this->assertEqual('residence-in-' . $message_id, $sanitized_message->oe_country_residence->target_id);
-    $this->assertEqual('+000-' . $message_id, $sanitized_message->oe_telephone->value);
-    $this->assertEqual('topic-' . $message_id, $sanitized_message->oe_topic->value);
+    $sanitized_message = Message::load($plain_message_id);
+
+    $this->assertEqual($sanitized_message->get('name')->value, $data['name']);
+    $this->assertEqual($sanitized_message->get('mail')->value, $data['mail']);
+    $this->assertEqual($sanitized_message->get('subject')->value, $data['subject']);
+    $this->assertEqual($sanitized_message->get('message')->value, $data['message']);
+    $this->assertEqual($sanitized_message->get('ip_address')->value, $data['ip_address']);
   }
 
 }
